d54c254c33284250b27476edce7f2966b990fa5c
Andrew <ajeo563@aucklanduni.ac.nz>
2025-08-15 15:49:24 +1200

    Created Settings component and SettingsPage that returns the Settings the component.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import EnterName from './pages/EnterName'
 import JoinRoom from './pages/JoinRoom'
 import InGamePage from './pages/InGamePage'
+import SettingsPage from './pages/SettingsPage'
+
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<EnterName />} />
         <Route path="/join" element={<JoinRoom />} />
         <Route path="/game" element={<InGamePage />} />
+        <Route path="/" element={<SettingsPage />} />
+
       </Routes>
     </Router>
   )
