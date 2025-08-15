55d8dc5510afed00a35c15e7ecf2abde45d02d8f
Andrew <ajeo563@aucklanduni.ac.nz>
2025-08-15 13:54:35 +1200

    Finished front-end implementation of the JoinRoom page.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import EnterName from './pages/EnterName.tsx'
+import JoinRoom from './pages/JoinRoom.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <EnterName />
+    <JoinRoom />
   </StrictMode>,
 )
