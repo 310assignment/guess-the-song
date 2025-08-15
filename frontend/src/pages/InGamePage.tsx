3b103ae3baf8e9b861044124f968a29965f715a6
ljun123@aucklanduni.ac.nz <ljun123@aucklanduni.ac.nz>
2025-08-15 16:01:40 +1200

    Created the in game page with a welcome message

---
+++ frontend/src/pages/InGamePage.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+interface GuessifyProps {}
+
+
+const InGamePage: React.FC<GuessifyProps> = () => {
+  return <h1>Welcome to the Game Page!</h1>;
+};
+
+export default InGamePage;
