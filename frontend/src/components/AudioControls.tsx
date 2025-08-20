c304359b42c9db9c284109165fa0c900b18c66ad
Kevin <kkim985@aucklanduni.ac.nz>
2025-08-20 19:08:44 +1200

    applied css to the audio controls

--- frontend/src/components/AudioControls.tsx
+++ frontend/src/components/AudioControls.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { songService } from '../services/songServices';
+import '../css/AudioControls.css';
 
 interface AudioControlsProps {
   className?: string;
